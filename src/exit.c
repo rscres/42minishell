85b61430dca4df417b4db8312141f67992cc88b4
renato <renatocrescenti@gmail.com>
2023-11-23 02:34:19 -0300

    start ft_exit() refactoring

--- src/exit.c
+++ src/exit.c
@@ -6,18 +6,26 @@
 /*   By: renato <renato@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 01:46:46 by renato            #+#    #+#             */
-/*   Updated: 2023/11/22 00:19:17 by renato           ###   ########.fr       */
+/*   Updated: 2023/11/23 01:13:04 by renato           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
+
+int	adjust_status(int status)
+{
+	while (status > 255)
+		status -= 256;
+	while (status < 0)
+		status += 256;
+	return (status);
+}
 
 //needs rework
 void	ft_exit(char **args, int argc)
 {
 	int		i;
 	int		status;
-	// char	*value;
 
 	clear_hashtable(g_main.env_var);
 	clear_token_list();
@@ -28,13 +36,6 @@ void	ft_exit(char **args, int argc)
 		exit(0);
 	}
 	i = 0;
-	// if (ft_strcmp(args[0], "+") || ft_strcmp(args[0], "-"))
-	// {
-	// 	i++;
-	// 	value = ft_strjoin(args[0], args[1]);
-	// 	clear_cmd_list();
-	// 	exit(ft_atoi(value));
-	// }
 	while (args[0][i])
 	{
 		if (!ft_isdigit(args[0][i]) && args[0][i] != '-' && args[0][i] != '+')
@@ -47,12 +48,7 @@ void	ft_exit(char **args, int argc)
 		}
 		i++;
 	}
-	status = 0;
-	status = ft_atoi(args[0]);
-	while (status > 255)
-		status -= 256;
-	while (status > 0)
-		status += 256;
+	status = adjust_status(ft_atoi(args[0]));
 	if (argc > 1)
 	{	
 		ft_putstr_fd("minishell: exit: too many arguments\n", 2);
@@ -69,7 +65,5 @@ void	ft_exit2(int status)
 	clear_cmd_list();
 	clear_token_list();
 	printf("exit\n");
-	while (status > 255)
-		status -= 256;
-	exit(status);
+	exit(adjust_status(status));
 }
