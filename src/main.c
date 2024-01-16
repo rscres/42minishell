e2c81ff3a0fb2a301bfe6b3f9fe660da16a271b9
rscres <rseelaen@student.42sp.org.br>
2024-01-16 19:07:35 -0300

    main.c: fix header

--- src/main.c
+++ src/main.c
@@ -1,63 +1,3 @@
-<<<<<<< HEAD
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rseelaen <rseelaen@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/27 11:58:19 by rseelaen          #+#    #+#             */
-/*   Updated: 2023/12/18 15:47:26 by rseelaen         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "../includes/shell.h"
-
-t_main	g_main;
-
-char	*get_line(void)
-{
-	char	*line;
-
-	line = readline("msh$ ");
-	if (!line)
-		return (NULL);
-	while (ft_strlen(line) > 0 && line[ft_strlen(line) - 1] == '\\')
-	{
-		line[ft_strlen(line) - 1] = '\0';
-		line = ft_strjoin(line, readline("> "));
-	}
-	return (line);
-}
-
-int	main(__attribute__((unused))int argc, __attribute__((unused))char **argv,
-            char **env)
-{
-	char	*line;
-
-	signal_set();
-	init_global();
-	set_env(g_main.env_var, env);
-	while (369)
-	{
-		line = get_line();
-		if (!line)
-			ft_exit2(g_main.status);
-		if (ft_strlen(line) > 0)
-		{
-			g_main.line = line;
-			lexer(&line);
-			parser();
-			execute_cmd_list();
-			clear_token_list();
-			clear_cmd_list();
-			add_history(g_main.line);
-		}
-		ft_safe_free((void **)&line);
-	}
-	return (g_main.status);
-}
-=======
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -70,7 +10,7 @@ int	main(__attribute__((unused))int argc, __attribute__((unused))char **argv,
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "shell.h"
+#include "../includes/shell.h"
 
 t_main	g_main;
 
@@ -124,13 +64,11 @@ char	*get_line(void)
 		line[ft_strlen(line) - 1] = '\0';
 		line = ft_strjoin(line, readline("> "));
 	}
-	// if (line)
-	// 	add_history(line);
 	return (line);
 }
 
 int	main(__attribute__((unused))int argc, __attribute__((unused))char **argv,
-	char **env)
+            char **env)
 {
 	char	*line;
 
@@ -138,7 +76,7 @@ int	main(__attribute__((unused))int argc, __attribute__((unused))char **argv,
 	signal_set();
 	init_global(env);
 	set_env(g_main.env_var, env);
-	while (1)
+	while (369)
 	{
 		line = get_line();
 		if (!line)
@@ -157,4 +95,3 @@ int	main(__attribute__((unused))int argc, __attribute__((unused))char **argv,
 	}
 	return (g_main.status);
 }
->>>>>>> main
