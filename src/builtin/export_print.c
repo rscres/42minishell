3737750c726ae5e084f45fa3174012b699013466
ISABELA GENIAL <isabelamgenial@gmail.com>
2024-02-09 13:08:46 -0300

    fix leak

--- src/builtin/export_print.c
+++ src/builtin/export_print.c
@@ -61,21 +61,25 @@ void	print_vars(int fd)
 	char	**tmp;
 	int		i;
 
-	i = -1;
+	i = 0;
+	signal(SIGPIPE, SIG_IGN);
 	array = save_table_to_array();
 	tmp = malloc(sizeof(char *) * 2);
 	if (!array)
 		return ;
 	ft_merge_sort(ft_arrlen(array), array);
-	while (array[++i])
+	while (array[i])
 	{
-		tmp[1] = ft_strchr(array[i], '=');
+		tmp[1] = ft_strdup(ft_strchr(array[i], '='));
 		tmp[0] = ft_strndup(array[i], ft_strlen(array[i])
 				- ft_strlen(tmp[1]));
 		print(tmp, fd);
 		ft_safe_free((void **)&tmp[0]);
+		ft_safe_free((void **)&tmp[1]);
+		i++;
 	}
 	tmp[1] = NULL;
 	ft_safe_free((void **)&tmp);
 	free_tab(array);
+	signal(SIGPIPE, SIG_DFL);
 }
--- src/pipe/pipe_utils.c
+++ src/pipe/pipe_utils.c
@@ -26,6 +26,9 @@ void	ig_pipe_closer(void)
 	ft_safe_free((void **) &g_main.pipe->path);
 	ft_safe_free((void **) &g_main.pipe);
 	free(g_main.line);
+	close(0);
+	close(1);
+	close(2);
 }
 
 void	ig_pipe_handler(t_cmd *cmd)
@@ -33,18 +36,12 @@ void	ig_pipe_handler(t_cmd *cmd)
 	if (file_dir_check(cmd))
 	{
 		ig_pipe_closer();
-		close(0);
-		close(1);
-		close(2);
 		exit(1);
 	}
 	if (check_if_builtin(cmd->name))
 	{
 		g_main.status = exec_builtin(cmd);
 		ig_pipe_closer();
-		close(0);
-		close(1);
-		close(2);
 		exit(g_main.status);
 	}
 	else if (g_main.pipe->path && !access(g_main.pipe->path, F_OK))
